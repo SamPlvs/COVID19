de58edd9dda3f20406cfba7516fe41bb87ffad14
S Tukra <35964759+SamPlvs@users.noreply.github.com>
2020-05-03 19:26:29 +0100

    Update argmuents.py

--- argmuents.py
+++ argmuents.py
@@ -5,12 +5,9 @@ def get_args():
     parser = argparse.ArgumentParser()
     # all arguments go here;
     parser.add_argument('--root_dir', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Images-processed', help='root director to the data')
-    parser.add_argument('--covid_train_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/COVID/trainCT_COVID.txt', help='root director to the data')
-    parser.add_argument('--covid_test_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/COVID/testCT_COVID.txt', help='root director to the data')
-    parser.add_argument('--covid_val_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/COVID/valCT_COVID.txt', help='root director to the data')
-    parser.add_argument('--healthy_train_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/NonCOVID/trainCT_NonCOVID.txt', help='root director to the data')
-    parser.add_argument('--healthy_val_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/NonCOVID/valCT_NonCOVID.txt', help='root director to the data')
-    parser.add_argument('--healthy_test_list', type=str, default='/home/mukund/Documents/COVID/COVID-CT/Data-split/NonCOVID/testCT_NonCOVID.txt', help='root director to the data')
-
+    parser.add_argument('--train_npy', type=str, default='./train_list.npy', help='root director to the data')
+    parser.add_argument('--test_npy', type=str, default='./test_list.npy', help='root director to the data')
+    parser.add_argument('--val_npy', type=str, default='./val_list.npy', help='root director to the data')
+    
     args= parser.parse_args()
     return args
